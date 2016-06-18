5050c6907ed0676386e9224500d343f596a314ef
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-06-19 00:37:02 +0200

    chore(update): Prepare for update

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ System.config({
     "optional": []
   },
   paths: {
-    "github:*": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.1/jspm_packages/github/*",
-    "npm:*": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.1/jspm_packages/npm/*",
-    "aurelia-bundle": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.1/dist/aurelia-bundle"
+    "github:*": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.2/jspm_packages/github/*",
+    "npm:*": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.2/jspm_packages/npm/*",
+    "aurelia-bundle": "https://cdn.rawgit.com/vegarringdal/vGridGistRunJSPMBundle/v.1.0.2/dist/aurelia-bundle"
   },
   bundles: {
     "aurelia-bundle": [
